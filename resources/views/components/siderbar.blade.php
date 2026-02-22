82c642f1f8688f91919f1d5d0ef03ac14f5db4c0
svkhk24 <tuananhcepdn@gmail.com>
2026-02-22 09:00:11 +0700

    second commit

--- resources/views/components/siderbar.blade.php
+++ resources/views/components/siderbar.blade.php
@@ -3,7 +3,7 @@
             <!-- SIDEBAR HEADER -->
             <div :class="sidebarToggle ? 'justify-center' : 'justify-between'"
                 class="flex items-center gap-2 pt-8 sidebar-header pb-7">
-                <a href="index.html">
+                <a href="{{ route('dashboard') }}">
                     <span class="logo" :class="sidebarToggle ? 'hidden' : ''">
                         <img class="dark:hidden" src="tailadmin/src/images/logo/logo.svg" alt="Logo" />
                         <img class="hidden dark:block" src="tailadmin/src/images/logo/logo-dark.svg" alt="Logo" />
@@ -86,31 +86,12 @@
                             </li>
                             <!-- Menu Item Dashboard -->
 
-                            <!-- Menu Item Calendar -->
-                            <li>
-                                <a href="calendar.html" @click="selected = (selected === 'Calendar' ? '':'Calendar')"
-                                    class="menu-item group"
-                                    :class="(selected === 'Calendar') && (page === 'calendar') ? 'menu-item-active' :
-                                    'menu-item-inactive'">
-                                    <svg :class="(selected === 'Calendar') && (page === 'calendar') ? 'menu-item-icon-active' :
-                                    'menu-item-icon-inactive'"
-                                        width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" clip-rule="evenodd"
-                                            d="M8 2C8.41421 2 8.75 2.33579 8.75 2.75V3.75H15.25V2.75C15.25 2.33579 15.5858 2 16 2C16.4142 2 16.75 2.33579 16.75 2.75V3.75H18.5C19.7426 3.75 20.75 4.75736 20.75 6V9V19C20.75 20.2426 19.7426 21.25 18.5 21.25H5.5C4.25736 21.25 3.25 20.2426 3.25 19V9V6C3.25 4.75736 4.25736 3.75 5.5 3.75H7.25V2.75C7.25 2.33579 7.58579 2 8 2ZM8 5.25H5.5C5.08579 5.25 4.75 5.58579 4.75 6V8.25H19.25V6C19.25 5.58579 18.9142 5.25 18.5 5.25H16H8ZM19.25 9.75H4.75V19C4.75 19.4142 5.08579 19.75 5.5 19.75H18.5C18.9142 19.75 19.25 19.4142 19.25 19V9.75Z"
-                                            fill="" />
-                                    </svg>
 
-                                    <span class="menu-item-text" :class="sidebarToggle ? 'lg:hidden' : ''">
-                                        Calendar
-                                    </span>
-                                </a>
-                            </li>
                             <!-- Menu Item Calendar -->
 
                             <!-- Menu Item Profile -->
                             <li>
-                                <a href="profile.html" @click="selected = (selected === 'Profile' ? '':'Profile')"
+                                <a href="{{ route('profile.edit') }}" @click="selected = (selected === 'Profile' ? '':'Profile')"
                                     class="menu-item group"
                                     :class="(selected === 'Profile') && (page === 'profile') ? 'menu-item-active' :
                                     'menu-item-inactive'">
@@ -124,7 +105,7 @@
                                     </svg>
 
                                     <span class="menu-item-text" :class="sidebarToggle ? 'lg:hidden' : ''">
-                                        User Profile
+                                        User
                                     </span>
                                 </a>
                             </li>
@@ -196,7 +177,7 @@
                                     </svg>
 
                                     <span class="menu-item-text" :class="sidebarToggle ? 'lg:hidden' : ''">
-                                        Tables
+                                        Products
                                     </span>
 
                                     <svg class="menu-item-arrow absolute right-2.5 top-1/2 -translate-y-1/2 stroke-current"
@@ -516,19 +497,6 @@
                 <!-- Sidebar Menu -->
 
                 <!-- Promo Box -->
-                <div :class="sidebarToggle ? 'lg:hidden' : ''"
-                    class="mx-auto mb-10 w-full max-w-60 rounded-2xl bg-gray-50 px-4 py-5 text-center dark:bg-white/[0.03]">
-                    <h3 class="mb-2 font-semibold text-gray-900 dark:text-white">
-                        #1 Tailwind CSS Dashboard
-                    </h3>
-                    <p class="mb-4 text-gray-500 text-theme-sm dark:text-gray-400">
-                        Leading Tailwind CSS Admin Template with 400+ UI Component and Pages.
-                    </p>
-                    <a href="https://tailadmin.com/pricing" target="_blank" rel="nofollow"
-                        class="flex items-center justify-center p-3 font-medium text-white rounded-lg bg-brand-500 text-theme-sm hover:bg-brand-600">
-                        Purchase Plan
-                    </a>
-                </div>
                 <!-- Promo Box -->
             </div>
         </aside>
